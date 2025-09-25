96bdcb66be0ceb6fccdf0076267bda591871d61a
LAPTOP-GL0BOCPU\onthe <elisha.roszell@gmail.com>
2025-09-24 19:03:49 -0700

    Add owner() and setPool(address) to HazardABI so owner console actions (setPool) work; resolves 'setPool is not a function'.

--- js/HazardABI.js
+++ js/HazardABI.js
@@ -1,10 +1,18 @@
 // Minimal ABI for dedicated Hazard contract (pooled payouts)
 (function(){
   window.HazardABI = [
+    // Core gameplay
     { "inputs": [{ "internalType": "uint8", "name": "main", "type": "uint8" }], "name": "playHazard", "outputs": [], "stateMutability": "payable", "type": "function" },
-    { "inputs": [], "name": "pool", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
+
+    // Views
+    { "inputs": [], "name": "pool",   "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "owner",  "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
     { "inputs": [], "name": "maxBet", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-    { "inputs": [], "name": "feeBps", "outputs": [{ "internalType": "uint16", "name": "", "type": "uint16" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "feeBps", "outputs": [{ "internalType": "uint16",  "name": "", "type": "uint16"  }], "stateMutability": "view", "type": "function" },
+
+    // Admin (owner only)
+    { "inputs": [{ "internalType": "address", "name": "poolAddr", "type": "address" }], "name": "setPool", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+
     { "anonymous": false, "inputs": [
         { "indexed": true, "internalType": "address", "name": "player", "type": "address" },
         { "indexed": false, "internalType": "uint256", "name": "wager", "type": "uint256" },
