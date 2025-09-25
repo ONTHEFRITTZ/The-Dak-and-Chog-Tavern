fb7a2d3f851e6eb1de86fa71c7d4b49456f1bd03
LAPTOP-GL0BOCPU\onthe <elisha.roszell@gmail.com>
2025-09-24 18:27:10 -0700

    PoolABI: add authorizedGames(address) view getter to allow verification in UI/console

--- js/PoolABI.js
+++ js/PoolABI.js
@@ -4,6 +4,7 @@
     { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" },
     { "inputs": [], "name": "owner", "outputs": [{"internalType":"address","name":"","type":"address"}], "stateMutability":"view", "type":"function" },
     { "inputs": [], "name": "paused", "outputs": [{"internalType":"bool","name":"","type":"bool"}], "stateMutability":"view", "type":"function" },
+    { "inputs": [{"internalType":"address","name":"game","type":"address"}], "name":"authorizedGames", "outputs": [{"internalType":"bool","name":"","type":"bool"}], "stateMutability":"view", "type":"function" },
     { "inputs": [{"internalType":"address","name":"game","type":"address"},{"internalType":"bool","name":"allowed","type":"bool"}], "name":"setAuthorized", "outputs": [], "stateMutability":"nonpayable", "type":"function" },
     { "inputs": [{"internalType":"bool","name":"p","type":"bool"}], "name":"pause", "outputs": [], "stateMutability":"nonpayable", "type":"function" },
     { "inputs": [{"internalType":"address","name":"newOwner","type":"address"}], "name":"transferOwnership", "outputs": [], "stateMutability":"nonpayable", "type":"function" },
