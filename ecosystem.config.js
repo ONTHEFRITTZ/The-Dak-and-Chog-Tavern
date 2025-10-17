ca320edda0016a044689f64bd77ff7434f999cd0
LAPTOP-GL0BOCPU\onthe <elisha.roszell@gmail.com>
2025-10-16 17:36:03 -0700

    llllleee

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
         GAME_TYPES: 'FARO,POKER',
         // Comma-separated, LOWERCASE wallet addresses with admin rights:
         ADMIN_ADDR: '0x8BA35Eca0fe68787b275C6ed065675829843Adf5',
-        POKER_DEALER_PK='0xed38c5cf958df74332edad1f147e767e14594e549f262722481ff05178f7e34d'
+        POKER_DEALER_PK=''
         HOLDEM_POKER_ADDR='0x424F89FE230331df8f656B683812b6394c323f17'
         MONAD_BUNDLER_RPC='https://monad-testnet.drpc.org'
         RT_RAKE_BPS: 100,
